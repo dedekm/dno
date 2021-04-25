f5f26c117d974156064e7e43254cfca45fd7b92e
dedekm <dedekmm@gmail.com>
2021-04-25 18:19:29 +0200

    add wall [wip]

--- src/Level.gd
+++ src/Level.gd
@@ -4,6 +4,6 @@ func _ready() -> void:
   Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
   pass
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
   if Input.is_action_just_pressed("ui_cancel"):
     Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
