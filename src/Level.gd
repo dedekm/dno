71ee9602c7fc6987a71747cb6c03a408fdaffb08
dedekm <dedekmm@gmail.com>
2021-04-26 22:48:25 +0200

    implement health & fall damage to player

--- src/Level.gd
+++ src/Level.gd
@@ -1,9 +1,34 @@
 extends Spatial
 
+onready var env : Environment = $WorldEnvironment.environment
+onready var red_fade_out := $RedFadeOut
+
+var filter_color := Color(1, 1, 1, 1)
+
 func _ready() -> void:
   Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+  pass
+
+func _process(_delta: float) -> void:
+  if filter_color:
+    env.adjustment_color_correction.gradient.colors[1] = filter_color
+
   pass
 
 func _input(_event: InputEvent) -> void:
   if Input.is_action_just_pressed("ui_cancel"):
     Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func add_red_filter(level: float) -> void:
+  env.adjustment_enabled = true
+  level = 1 - level
+  red_fade_out.interpolate_property(self,
+                                    "filter_color",
+                                    filter_color,
+                                    Color(1, level, level, 1),
+                                    2.0,
+                                    Tween.TRANS_ELASTIC,
+                                    Tween.EASE_OUT)
+
+  red_fade_out.start()
--- src/character/Character.gd
+++ src/character/Character.gd
@@ -3,6 +3,11 @@ extends RigidBody
 # constants
 const Ladder = preload("res://src/Ladder.tscn")
 
+# gameplay
+export(int, 1, 10) var health := 2
+export(int, 1, 10) var injured_height := 2
+export(int, 1, 10) var killed_height := 4
+
 # mouse sensitivity
 export(float,0.1,1.0) var sensitivity_x = 0.5
 export(float,0.1,1.0) var sensitivity_y = 0.4
@@ -12,6 +17,7 @@ export(float,1.0, 20.0) var speed = 10.0
 export(float,1.0, 30.0) var jump_speed = 5
 
 # instance refs
+onready var level := get_parent()
 onready var face := $Face
 onready var hands := $Face/Hands
 onready var camera := $Face/Camera
@@ -20,20 +26,32 @@ onready var ladder_timer := $LadderTimer
 
 # variables
 var mouse_motion := Vector2()
-var jump := false
+var on_ground := false
+var last_solid_y_position : float
 
 var ladder : Ladder
 
 func _ready() -> void:
+  last_solid_y_position = global_transform.origin.y
   Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
   ground_ray.enabled = true
   pass
 
-func _integrate_forces(state: PhysicsDirectBodyState) -> void:
+func _integrate_forces(_state: PhysicsDirectBodyState) -> void:
   var direction_force := get_direction_force()
+  
+  var just_landed : bool = ground_ray.is_colliding()
+  if just_landed and !on_ground:
+    var fallen_height := last_solid_y_position - global_transform.origin.y
+    apply_fall_damage(fallen_height)
+
+    print("jumped " + String(fallen_height))
+    last_solid_y_position = global_transform.origin.y
+  
+  on_ground = just_landed
 
   var jumping := 0
-  if Input.is_action_just_pressed("jump") and ground_ray.is_colliding():
+  if Input.is_action_just_pressed("jump") and on_ground:
     jumping = 1
 
   set_axis_velocity(Vector3(0, jumping * jump_speed, 0))
@@ -50,7 +68,6 @@ func _physics_process(delta: float) -> void:
   pass
 
 func _input(event: InputEvent) -> void:
-
   if event is InputEventMouseMotion:
     mouse_motion = event.relative
 
@@ -79,6 +96,24 @@ func _input(event: InputEvent) -> void:
 func add_ladder_part() -> void:
   if ladder:
     ladder.add_part()
+    
+func apply_fall_damage(height: float) -> void:
+  var h := health
+
+  if height >= killed_height:
+    level.add_red_filter(0.9)
+    h = 0
+  elif height >= injured_height:
+    h -= 1
+    
+  if h <= 0:
+    # killed
+    level.add_red_filter(0.9)
+  elif h != health:
+    # injured
+    level.add_red_filter(0.3)   
+  
+  health = h
 
 func get_direction_force() -> Vector3:
   var m := Vector3()
